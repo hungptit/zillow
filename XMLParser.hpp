3afaf93a6b157b338146d7960a20c020659f4b0a
hungdang <hungptit@gmail.com>
2016-06-07 16:04:41 -0400

    Add XMLParser header which will have all classes functions needed for parsing zillow XML data.

---
+++ XMLParser.hpp
@@ -0,0 +1,15 @@
+#ifndef XMLParser_hpp_
+#define XMLParser_hpp_
+
+#include "fmt/format.h"
+#include "pugixml.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace zillow {
+    class DeepSearchResults {
+};
+}
+
+#endif
